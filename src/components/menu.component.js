707fb14115f181dce5fc568091d03a1ba0b1b258
Carlos <pcass78@gmail.com>
2018-07-27 16:27:16 -0600

    all the code

---
+++ src/components/menu.component.js
@@ -0,0 +1,12 @@
+/**
+ * Created by csalas on 7/24/18.
+ */
+import React, { Component } from 'react';
+
+const MenuComponent  = function () {
+        return (
+           <div>Menu</div>
+        );
+}
+
+export default MenuComponent;
