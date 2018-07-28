668957399e13a11160c895c6bf1d1f1f7a04449d
Carlos <pcass78@gmail.com>
2018-07-28 13:45:14 -0600

    add source code

---
+++ src/components/menu.component.js
@@ -0,0 +1,15 @@
+/**
+ * Created by csalas on 7/24/18.
+ */
+import React, { Component } from 'react';
+import image from '../d.png';
+
+const MenuComponent  = function () {
+        return (
+           <div>
+                <img className="logod" src={image} />
+           </div>
+        );
+}
+
+export default MenuComponent;
